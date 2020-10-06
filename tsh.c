1b2c0db8c319b51b72ecb1ca82e5ff98d5ca72cc
Sarobidy RAPETERAMANA <sarobidy@OnePlus6-1.home>
2020-10-06 12:06:49 +0200

    read the terminal input and exit

--- tsh.c
+++ tsh.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <string.h>
+#define BUFSIZE 512
+char read_buff[BUFSIZE]; //buff for the read
+
 int main(int nb, char **args){
-    if(nb<2){
+    if(nb<2){ //if missing the .tar
         printf("missing the .tar file to execute this programe\n");
         return -1;
+    }
+    while(1){
+        read(0, read_buff, BUFSIZE);//user write his command on the input
+        read_buff[strlen(read_buff)-1] = '\0';
+        if(strcmp("exit", read_buff)==0)break;
+        /*
+            if the command wasn't exit
+        */
     }
     return 0;
 }
