60bf15b9eef7a66bb3c10843873aac47959cb7a6
Sarobidy RAPETERAMANA <sarobidy@openroam-prg-hf-1-135-94.net.univ-paris-diderot.fr>
2020-10-09 17:08:23 +0200

    create the getIntoDirectory

---
+++ cd.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "tar.h"
+/*char * concatString(char * path, char *dir){
+    char * result = mallo
+}*/
+void getIntoDirectory(int descripteur, char * PATH, char * directory){
+    lseek(descripteur, 0, SEEK_SET);
+    struct posix_header *tete = malloc(512);
+    int nb_bloc_fichier = 0;
+    while(read(descripteur, tete, 512)>0){//parcour de tete en tete jusqu' a la fin
+
+
+        int tmp = 0;
+        sscanf(tete->size, "%o", &tmp);
+        nb_bloc_fichier = (tmp + 512 -1) / 512;
+        for(int i=0; i<nb_bloc_fichier; i++){
+            read(descripteur, tete, 512);
+        }
+    }
+}
+void cd(int descripteur, char * PATH, char * directory){
+
+}
+int main(void){
+    return 0;
+}
