905ea27dd161288bad3ae7eaf8eab033b51b5546
Sarobidy RAPETERAMANA <sarobidy@OnePlus6-1.home>
2020-10-10 13:22:08 +0200

    create a struct tsh_memory to keep all the information about the tar traitement in the same place

--- Makefile
+++ Makefile
@@ -3,10 +3,11 @@ CFLAGS= -Wall -g
 LDLIBS= -lm
 
 
-tsh : tsh.o cd.o
+tsh : tsh.o cd.o tsh_memory.o
 
-tsh.o : tsh.c cd.h
+tsh.o : tsh.c cd.h tsh_memory.h
 cd.o : cd.c tar.h
+tsh_memory.o : tsh_memory.c tsh_memory.h
 
 cleanall:
 	rm -rf *.o main *~ 
--- tsh.c
+++ tsh.c
@@ -5,18 +5,22 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/errno.h>
+#include "tsh_memory.h"
 #include "cd.h"
 
 #define BUFSIZE 512
+tsh_memory * memory;
 char read_buff[BUFSIZE]; //buff for the read
 char PATH[1024];//representing the relative path from the tar
-char *getCommand(){
-    return read_buff + strlen(PATH);
-}
-void update_path(int path_descriptor){
+/*char ** getCommand(){//so we can pass it to exec
+    char * debut_comande = read_buff + strlen(PATH);
+    //comand arg1 arg2 ......
+    //apres on cree juste un processus fils puis executer le programe
+}*/
+/*void update_path(int path_descriptor){
     lseek(path_descriptor, 0, SEEK_SET);
     read(path_descriptor, PATH, BUFSIZE);
-}
+}*/
 int main(int nb, char **args){
     if(nb<2){ //if missing the .tar
         //should check also if it's not a .tar
@@ -26,6 +30,7 @@ int main(int nb, char **args){
     //we open our .tar in order to execute command on it
     int fd_tar = open(args[1], O_RDWR);
     int fd_path = open("PATH.txt", O_RDWR);
+    memory = instanciate_tsh_memory(fd_path, fd_tar);
     if(errno == ENOENT){//no such file
         perror("");
         return -1;
@@ -35,8 +40,8 @@ int main(int nb, char **args){
     }
 
     while(1){
-        update_path(fd_path);
-        write(1, PATH, strlen(PATH));
+        update_path(memory);
+        write(1, memory->PATH, strlen(memory->PATH));
         read(0, read_buff, BUFSIZE);//user write his command on the input
         read_buff[strlen(read_buff)-1] = '\0';
         if(memmem(read_buff, strlen(read_buff), "exit", 4))break;
