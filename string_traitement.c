12634a005572f9b581ae45157458a8432ab075bf
Sarobidy RAPETERAMANA <sarobidy@OnePlus6-1.home>
2020-10-11 21:29:09 +0200

    create a count_arg methode in order to count the substring in the char*

--- string_traitement.c
+++ string_traitement.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 int count_args(char *args){
     int counter = 0;
-    int if_space = 0;
     int get_into_char = 0;
     int length = strlen(args);
     for(int i=0; i<length; i++){
         if(args[i]!=' ' && get_into_char==0){//we read the first char or a word
             counter++;
+            get_into_char = 1;
+        }
+        if(args[i]==' '){
+            get_into_char = 0;
         }
     }
-    return 0;
+    return counter;
 }
 /*char ** getCommand(int *nb, char *args){
 
 }*/
 int main(void){
-    printf("teste");
+    printf("tail = %d\n", count_args(" cd   System/   cours"));
     return 0;
 }
