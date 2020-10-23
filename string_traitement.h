2b60054be6cdb1d66dfb3901b4f62ff0d34c757e
Sarobidy RAPETERAMANA <sarobidy@ipad-de-claude.home>
2020-10-24 01:38:50 +0200

    export the getTarName

--- string_traitement.h
+++ string_traitement.h
@@ -4,4 +4,5 @@ extern int string_to_int(char *chiffre);
 extern char *int_to_string(int chiffre);
 extern char * concatString(char * path, char *dir);
 extern void getPreTar(char *initial_string, char *result);
+extern void getTarName(char *initial_string, char *result);
 #endif
