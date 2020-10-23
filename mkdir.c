475dfa1cd27b2e0ad94323cdfc495455f3ff8fe4
Sarobidy RAPETERAMANA <sarobidy@openroam-prg-hf-1-132-110.net.univ-paris-diderot.fr>
2020-10-23 18:51:15 +0200

    reussi

--- mkdir.c
+++ mkdir.c
@@ -16,7 +16,7 @@ struct posix_header *create_header(char * name){
     sprintf(result->mtime, "%011o", 1603298969);
      
     result->mtime[0] = '\0';//how to get the time of the arrchivement
-    set_checksum(result);
+    
     result->typeflag = '5';
     result->linkname[0] = '\0';   
     
@@ -31,32 +31,8 @@ struct posix_header *create_header(char * name){
     strcpy(result->devminor, "000000 ");             
     result->prefix[0] = '\0';             
     result->junk[0]= '\0';  
+    set_checksum(result);
     return result;
-}
-void printHead(struct posix_header *result){
-    printf("name = -%s-\n", result->name);//add the name
-    printf("mode = -%s-\n", result->mode);
-     
-    printf("uid = -%s-\n", result->uid);//how to get the id of the creator
-    
-    printf("gid = -%s-\n", result->gid);//how to get the id of the group
-
-    printf("size = -%s-\n",result->size);  
-    printf("mtime = -%s-\n", result->mtime);//how to get the time of the arrchivement
-    printf("chksum = -%s-\n", result->chksum);
-    printf("typeflag = -%c-\n", result->typeflag);
-    printf("linkname = -%s-\n", result->linkname);   
-    
-    printf("magic = -%s-\n", result->magic); 
-    printf("version = -%s-\n", result->version);
-           
-    printf("uname = -%s-\n", result->uname);//how to get the user name
-    printf("gname = -%s-\n", result->gname);//how to get group name
-    
-    printf("devmajor = -%s-\n",result->devmajor);
-    printf("devminor = -%s-\n",result->devminor);           
-    printf("prefix = -%s-\n", result->prefix);    
-    printf("junk = -%s-\n", result->junk);              
 }
 int end_bloc(struct posix_header *header){
     //create a string that has all the bloc zero byte, in order to compare with header
@@ -65,29 +41,6 @@ int end_bloc(struct posix_header *header){
     //then compare
     if(memcmp(header, end_bloc, 512) == 0)return 1;//identical
     return 0;
-}
-void affiche(int descriptor){
-    lseek(descriptor, 0, SEEK_SET);
-    struct posix_header *header = malloc(512);
-    int nb_bloc_file = 0;
-    int j = 1;
-    while(read(descriptor, header, 512)>0){
-        printf("name = %s\n", header->name);
-        if(end_bloc(header)){//the moment we check the first bloc, we return to the back because we have read it
-            printf("--end\n");
-        }
-        int tmp = 0;
-        int time;
-        sscanf(header->size, "%o", &tmp);
-        sscanf(header->mtime, "%o", &time);
-        printf("time = %d\n", time);
-        nb_bloc_file = (tmp + 512 -1) / 512;
-        for(int i=0; i<nb_bloc_file; i++){
-            printf("   %d em bloc contenu\n", i+1);
-            read(descriptor, header, 512);
-        }
-        j++;
-    }
 }
 void put_at_the_first_null(int descriptor){
     lseek(descriptor, 0, SEEK_SET);
@@ -119,15 +72,11 @@ void mkdir2(char *dir_name, int tar_descriptor){
     char end_bloc[512];
     memset(end_bloc, 0, 512);
     write(tar_descriptor, end_bloc, 512);
-    affiche(tar_descriptor);
+    //affiche(tar_descriptor);
 
 }
 int main(int n, char** args){
     //directory/ nomDuTar
     int ouverture = open(args[2], O_RDWR );
     mkdir2(args[1], ouverture);
-    //affiche(ouverture);
-    /*struct posix_header *new_head = malloc(512);
-    read(ouverture, new_head, 512);
-    printHead(new_head);*/
 }
