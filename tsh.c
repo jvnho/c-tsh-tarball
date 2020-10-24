4170d626ffabf43d71afc11507304f15fa6d59fb
Sarobidy RAPETERAMANA <sarobidy@ipad-de-claude.home>
2020-10-24 10:24:18 +0200

    commit befor create a debug branch

--- tsh.c
+++ tsh.c
@@ -16,14 +16,18 @@ tsh_memory * memory;
 int main(void){
     //we create a memory about the current state so all processu can relate on it
     if((memory = create_memory())==NULL)return -1;
-    while(1){
+    /*while(1){
         PATH = getPath(memory);
         write(1, PATH, strlen(PATH));
         read(0, memory->comand, MAX_COMMAND);//user write his command on the input
         memory->comand[strlen(memory->comand)-1] = '\0';
         if(memmem(memory->comand, strlen(memory->comand), "exit", 4))break;
-    }
-
+    }*/
+    PATH = getPath(memory);
+    write(1, PATH, strlen(PATH));
+    printf("result cd = %d\n", cd("test2", memory));
+    PATH = getPath(memory);
+    write(1, PATH, strlen(PATH));
 
     free_tsh_memory(memory);
     return 0;
