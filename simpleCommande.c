ebe36ba77f2185f8fa0f496b4c7905c64d0f830a
Sarobidy RAPETERAMANA <sarobidy@ipad-de-claude.home>
2020-11-05 23:28:05 +0100

    teste a function

--- simpleCommande.c
+++ simpleCommande.c
@@ -1,10 +1,11 @@
 #include <string.h>
+#include <stdio.h>
 #include "tsh_memory.h"
 #include "cd.h"
 #include "pwd.h"
 #include "mkdir.h"
 #include "ls.h"
-char **listCommande = {"cd", "pwd", "mkdir", "ls"};
+char *listCommande[] = {"cd", "pwd", "mkdir", "ls"};
 #define NB_FUN 4
 int getFuncitonIndex(char *name){
     for(int i=0; i<NB_FUN; i++){
@@ -13,5 +14,6 @@ int getFuncitonIndex(char *name){
     return -1;
 }
 int execSimpleCommande(char *commande){
+    printf("indice = %d\n", getFuncitonIndex("ls"));
     return 0;
 }
