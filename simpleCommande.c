f59b302b9c200276548ecc97ed6a3cb48a47d165
Sarobidy RAPETERAMANA <sarobidy@ipad-de-claude.home>
2020-11-05 23:22:09 +0100

    create a tab full of the commande and getFunIndex to get the index in the tab

--- simpleCommande.c
+++ simpleCommande.c
@@ -1,8 +1,17 @@
+#include <string.h>
 #include "tsh_memory.h"
 #include "cd.h"
 #include "pwd.h"
 #include "mkdir.h"
 #include "ls.h"
+char **listCommande = {"cd", "pwd", "mkdir", "ls"};
+#define NB_FUN 4
+int getFuncitonIndex(char *name){
+    for(int i=0; i<NB_FUN; i++){
+        if(strcmp(name, listCommande[i])==0)return i;
+    }
+    return -1;
+}
 int execSimpleCommande(char *commande){
     return 0;
 }
