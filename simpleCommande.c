228de54828ba5352b5506b9c14254017004e1ee4
Sarobidy RAPETERAMANA <sarobidy@ipad-de-claude.home>
2020-11-06 18:33:07 +0100

    make the argument in pointer function work

--- simpleCommande.c
+++ simpleCommande.c
@@ -9,35 +9,14 @@
 #include "string_traitement.h"
 char *listCommande[] = {"cd", "pwd", "mkdir"};
 #define NB_FUN 3
-
-
-int getFuncitonIndex(char *name){
-    for(int i=0; i<NB_FUN; i++){
-        if(strcmp(name, listCommande[i])==0)return i;
-    }
-    return -1;
-}
-int adapter_cd(tsh_memory *memory, char **args){
-    printf("%s\n", args[0]);
-    return cd(args[0], memory);
-}
-int adapter_pwd(tsh_memory *memory, char **args){
-    return pwd(memory);
-}
-int adapter_mkdir(tsh_memory *memory, char **args){
-    return mkdir(args[0], memory);
-}
-typedef int (*pt_adapter) (tsh_memory *memory, char **args);
-pt_adapter listFun [NB_FUN] = { adapter_cd, adapter_pwd, adapter_mkdir};
-int execSimpleCommande(char *commande, tsh_memory *memory){
-    int nb_args = count_args(commande);
-    int i_args = 0;
-    //fill the token
-    char args[nb_args][50];
-    char com[80];
+char args[50][50];
+int i_args = 0;
+const char space[2] = " ";
+void fillArgs(char *commande){
+    //fill by token
+    char com[512];
     strcpy(com, commande);
     com[strlen(commande)] = '\0';
-    const char space[2] = " ";
     char *read;
     read = strtok(com, space);
     if(read!=NULL){
@@ -50,6 +29,26 @@ int execSimpleCommande(char *commande, tsh_memory *memory){
         i_args++;
         read = strtok(NULL, space);
     }
-    (*(listFun[getFuncitonIndex(args[0])]))(memory, args+1);
+}
+int adapter_cd(tsh_memory *memory){
+    return cd(args[1], memory);
+}
+int adapter_pwd(tsh_memory *memory){
+    return pwd(memory);
+}
+int adapter_mkdir(tsh_memory *memory){
+    return mkdir(args[1], memory);
+}
+typedef int (*pt_adapter) (tsh_memory *memory);
+pt_adapter listFun [NB_FUN] = { adapter_cd, adapter_pwd, adapter_mkdir};
+int getFuncitonIndex(char *name){
+    for(int i=0; i<NB_FUN; i++){
+        if(strcmp(name, listCommande[i])==0)return i;
+    }
+    return -1;
+}
+int execSimpleCommande(char *commande, tsh_memory *memory){
+    fillArgs(commande);
+    (*(listFun[getFuncitonIndex(args[0])]))(memory);
     return 0;
 }
