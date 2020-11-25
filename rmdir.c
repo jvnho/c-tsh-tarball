5ed04e877c73f3d3307a345c09138e9465f0bc76
Nicolas Sangsiri <nicolassangsiri@gmail.com>
2020-11-25 14:19:28 +0100

    rmdir.c: using directly number of bytes instead of number of blocks

--- rmdir.c
+++ rmdir.c
@@ -32,8 +32,7 @@ int rmdir_func(tsh_memory *mem, char *dir){
 
 int rmdir_in_tar(int fd, char* full_path){
     struct posix_header hd;
-    off_t file_offset = 0; //offset will allow to start reading from a certain position
-    off_t final_size = 0; //will help us now how much we truncate
+    off_t file_offset = 0, final_size = 0; //offset will allow to start reading from a certain position
     int occurrence = occ_counter_path(fd, full_path, &file_offset, &final_size);
     if(occurrence != 1){
         char s[] = "Directory is not empty or does not exist.\n";
@@ -48,31 +47,30 @@ int rmdir_in_tar(int fd, char* full_path){
         lseek(fd, BLOCKSIZE, SEEK_CUR); //repositionning the offset to the next replacing block
     }
     //procedure to truncate the tar
-    final_size = (final_size-1) * 512;
     lseek(fd,0,SEEK_SET);
-    ftruncate(fd, final_size);
+    ftruncate(fd, final_size-512); // -512 because we replace a header by a zero byte block so we can delete it
     return 1;
 }
 
 //returns the number of times the path appears in the tarball
 //returns to a pointer 'file_offset' the position of the rep and the number of blocks found
-int occ_counter_path(int fd, char* full_path, off_t* file_offset, off_t *nb_bloc){
+int occ_counter_path(int fd, char* full_path, off_t* file_offset, off_t *tar_size){
     lseek(fd, 0, SEEK_SET);
     int occurence = 0;
     struct posix_header hd;
     while(read(fd, &hd, 512) > 0){//reading the entire tarball
-        int filesize = 0;
-        sscanf(hd.size, "%o", &filesize);
-        int nb_bloc_fichier = (filesize + 512 -1) / 512;
-        (*nb_bloc) += 1+nb_bloc_fichier;
         if(strncmp(hd.name, full_path, strlen(full_path)) == 0){
             occurence++;
             if(hd.typeflag == '5'){
                 *file_offset = lseek(fd,0,SEEK_CUR);//position of the blocks RIGHT NEXT to the one the user wants to delete
             }
         }
+        int filesize = 0;
+        sscanf(hd.size, "%o", &filesize);
+        int nb_content_block = (filesize + 512 -1) / 512;
 
-        lseek(fd,nb_bloc_fichier*512, SEEK_CUR); //allow to jump to the next header block
+        (*tar_size) += (1+nb_content_block)*512;
+        lseek(fd,nb_content_block*512, SEEK_CUR); //allow to jump to the next header block
     }
     return occurence;
 }
