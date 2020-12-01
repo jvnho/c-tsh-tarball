1fa3ed7b9de96ed846146ca0e297eddd936821e7
Nicolas Sangsiri <nicolassangsiri@gmail.com>
2020-12-01 16:30:58 +0100

    rm.c: changed funcs order

--- rm.c
+++ rm.c
@@ -1,49 +1,75 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 #include "tar.h"
 #include "string_traitement.h"
 #include "tsh_memory.h"
+#include "cd.h"
 
-int rm_aux(int, char*,int*,off_t*,int);
-int is_a_end_bloc(struct posix_header*);
-int number_of_block(int fd);
+tsh_memory old_memory;
 
-void exec_rm(char *dir, char option[50][50]){
-    int r = fork();
-    if(r == 0)
-    execlp("rm", "rm", dir, NULL);
-    else wait(NULL);
+//create a 512 bytes block of zero byte, in order to compare with header
+int is_a_end_bloc(struct posix_header *header){
+    char end_bloc[512];
+    memset(end_bloc, 0, 512);
+    if(memcmp(header, end_bloc, 512) == 0) return 1; //identical
+    return 0;
 }
 
-int rm(tsh_memory *memory, char args[50][50], int nb_arg, char option[50][50],int nb_option){
-    char location[512];
-    for(int i = 0; i < nb_arg; i++){
-        saveMemory(memory, &old_memory);
-        getLocation(args[i], location); //check string_traitement for details
-        int lenLocation = strlen(location);
-        if(lenLocation > 0){//if there is an extra path cd to that path
-            if(cd(location, memory) == -1) return -1;
-        }
-        char *dirToDelete = args[i] + lenLocation;
-        if(in_a_tar(memory) == 1){
-            char *path_to_dir = concatDirToPath(memory->FAKE_PATH, dirToDelete);
-            rmdir_in_tar(atoi(memory->tar_descriptor),path_to_dir,1);
-        } else exec_rmdir(dirToDelete, option);
+//return the number total of block in a tar (i.e its size)
+int number_of_block(int fd){
+    char buffer[512];
+    int ret = 0;
+    lseek(fd, 0, SEEK_SET);
+    while(read(fd, buffer, 512) > 0)
+        ret++;
+    return ret;
+}
 
-        //restoring the last state of the memory
-        saveMemory(&old_memory, memory);
-        char *destination = malloc(strlen(memory->REAL_PATH));
-        strncpy(destination, memory->REAL_PATH, strlen(memory->REAL_PATH)-2);
-        cd(destination,memory); //cd-ing back to where we were
+//function that counts the number of content block to delete
+//returns -1 if such file/directory is not found
+int rm_in_tar_aux(int fd, char* full_path, int *nb_block_to_delete, off_t *debut, int arg_r){
+    lseek(fd, 0, SEEK_SET);
+
+    int filesize = 0;
+    struct posix_header hd;
+
+    while(read(fd, &hd, BLOCKSIZE) > 0){
+        sscanf(hd.size, "%o", &filesize);
+        int nb_bloc_fichier = (filesize + BLOCKSIZE -1) / BLOCKSIZE;
+        if(strncmp(hd.name, full_path, strlen(full_path)) == 0){
+            *debut = lseek(fd, 0, SEEK_CUR);
+            if(arg_r == 0){
+                if(hd.typeflag != '5'){ //if it's not a directory
+                    *nb_block_to_delete = nb_bloc_fichier;
+                    return 1;
+                }
+            } else { //argument -r was given as option
+                while(strncmp(hd.name, full_path,strlen(full_path)) == 0){
+                    sscanf(hd.size, "%o", &filesize);
+                    nb_bloc_fichier = (filesize + BLOCKSIZE -1) / BLOCKSIZE;
+                    *nb_block_to_delete += 1 + nb_bloc_fichier;
+
+                    lseek(fd, nb_bloc_fichier*512, SEEK_CUR);//jump to the next header
+                    read(fd,&hd, BLOCKSIZE);
+                }
+                *nb_block_to_delete -= 1; //not counting the first header (fd starting next to the first header)
+                return 1;
+            }
+        }
+        lseek(fd, nb_bloc_fichier*512, SEEK_CUR); //jumping to the next file header
     }
-    return 1;
+    return -1;//no such file
 }
 
 //full_path is the concatenation of PATH and the "target" user wants to delete (rm)
 int rm_in_tar(int fd, char* full_path, int arg_r, int first_call){
     int nb_content_bloc = 0;
     off_t file_offset = 0, tar_size = 0;
-    if(rm_aux(fd, full_path, &nb_content_bloc,&file_offset, arg_r) < 0 ){
+    if(rm_in_tar_aux(fd, full_path, &nb_content_bloc,&file_offset, arg_r) < 0 ){
         if(first_call == 1){
             char *msg;
             if(arg_r == 0) msg = "File not found or target is a directory.\n";
@@ -81,56 +107,42 @@ int rm_in_tar(int fd, char* full_path, int arg_r, int first_call){
      }
 }
 
-//function that counts the number of content block to delete
-//returns -1 if such file/directory is not found
-int rm_aux(int fd, char* full_path, int *nb_block_to_delete, off_t *debut, int arg_r){
-    lseek(fd, 0, SEEK_SET);
-
-    int filesize = 0;
-    struct posix_header hd;
-
-    while(read(fd, &hd, BLOCKSIZE) > 0){
-        sscanf(hd.size, "%o", &filesize);
-        int nb_bloc_fichier = (filesize + BLOCKSIZE -1) / BLOCKSIZE;
-        if(strncmp(hd.name, full_path, strlen(full_path)) == 0){
-            *debut = lseek(fd, 0, SEEK_CUR);
-            if(arg_r == 0){
-                if(hd.typeflag != '5'){ //if it's not a directory
-                    *nb_block_to_delete = nb_bloc_fichier;
-                    return 1;
-                }
-            } else { //argument -r was given as option
-                while(strncmp(hd.name, full_path,strlen(full_path)) == 0){
-                    sscanf(hd.size, "%o", &filesize);
-                    nb_bloc_fichier = (filesize + BLOCKSIZE -1) / BLOCKSIZE;
-                    *nb_block_to_delete += 1 + nb_bloc_fichier;
-
-                    lseek(fd, nb_bloc_fichier*512, SEEK_CUR);//jump to the next header
-                    read(fd,&hd, BLOCKSIZE);
-                }
-                *nb_block_to_delete -= 1; //not counting the first header (fd starting next to the first header)
-                return 1;
-            }
-        }
-        lseek(fd, nb_bloc_fichier*512, SEEK_CUR); //jumping to the next file header
-    }
-    return -1;//no such file
+void exec_rm(char *dir, char option[50][50]){
+    int r = fork();
+    if(r == 0)
+    execlp("rm", "rm", dir, NULL);
+    else wait(NULL);
 }
 
-//create a 512 bytes block of zero byte, in order to compare with header
-int is_a_end_bloc(struct posix_header *header){
-    char end_bloc[512];
-    memset(end_bloc, 0, 512);
-    if(memcmp(header, end_bloc, 512) == 0) return 1; //identical
+int option_R_present(char option[50][50], int nb_option){
+    for(int i = 0; i < nb_option; i++){
+        if(strcmp(option[i],"-R") == 0 || strcmp(option[i],"-r") == 0)
+            return 1;
+    }
     return 0;
 }
 
-//return the number total of block in a tar (i.e its size)
-int number_of_block(int fd){
-    char buffer[512];
-    int ret = 0;
-    lseek(fd, 0, SEEK_SET);
-    while(read(fd, buffer, 512) > 0)
-        ret++;
-    return ret;
+int rm(tsh_memory *memory, char args[50][50], int nb_arg, char option[50][50],int nb_option){
+    char location[512];
+    for(int i = 0; i < nb_arg; i++){
+        copyMemory(memory, &old_memory);
+        getLocation(args[i], location); //check string_traitement for details
+        int lenLocation = strlen(location);
+        if(lenLocation > 0){//if there is an extra path cd to that path
+            if(cd(location, memory) == -1) return -1;
+        }
+        char *dirToDelete = args[i] + lenLocation;
+        if(in_a_tar(memory) == 1){
+            char *path_to_dir = concatDirToPath(memory->FAKE_PATH, dirToDelete);
+            int option_r = option_R_present(option, nb_option);
+            rm_in_tar(atoi(memory->tar_descriptor),path_to_dir,option_r,1);
+        } else exec_rm(dirToDelete, option);
+
+        //restoring the last state of the memory
+        copyMemory(&old_memory, memory);
+        char *destination = malloc(strlen(memory->REAL_PATH));
+        strncpy(destination, memory->REAL_PATH, strlen(memory->REAL_PATH)-2);
+        cd(destination,memory); //cd-ing back to where we were
+    }
+    return 1;
 }
--- rm.h
+++ rm.h
@@ -1,5 +1,7 @@
 #ifndef RM_H
 #define RM_H
 
+#include "tsh_memory.h"
+
 extern int rm(tsh_memory *memory, char args[50][50], int nb_arg, char option[50][50],int nb_option);
 #endif
