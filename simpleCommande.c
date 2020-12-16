83724c30d41ccfeda9e2d4e49f128e77d0565130
Sarobidy RAPETERAMANA <sarobidy@oneplus7-1.home>
2020-12-16 19:17:39 +0100

    separate the file

--- simpleCommande.c
+++ simpleCommande.c
@@ -13,7 +13,7 @@
 #include "rmdir.h"
 #include "string_traitement.h"
 #include "rm.h"
-
+#include "pipe.h"
 
 
 char *listCommande[] = {"exit", "cd", "pwd", "mkdir", "ls", "rmdir", "rm"};
@@ -25,7 +25,7 @@ char args[50][50];
 int i_args = 0;
 const char space[2] = " ";
 int returnval;
-int save_read_fd;
+
 
 void fillArgs(char *commande){
     //fill by token
@@ -196,39 +196,7 @@ void printError(tsh_memory *memory, int error){
     sprintf(message, "tsh : command not found: %s\n", memory->comand);
     if(error == 127)write(2, message, strlen(message));
 }
-int pipe_tsh(tsh_memory *memory1, tsh_memory *memory2){
-    
-    save_read_fd = dup(0);
-    int fd_pipe[2];
-    if(pipe(fd_pipe)==-1){
-        perror("pipe:");
-        return -1;
-    }
-    int pid_fils = fork();
-    if(pid_fils){//Parent (read)
-        close(fd_pipe[1]);
-        dup2(fd_pipe[0], 0);
-        close(fd_pipe[0]);
-        int status;
-        waitpid(pid_fils, &status, 0);
-        if(WIFEXITED(status)){//if the child used exit()
-            if(WEXITSTATUS(status)){//if the number passed by exit is not 0, then the child finished with error
-                dup2(save_read_fd, 0);
-                printf("here = %d\n", WEXITSTATUS(status));
-                return WEXITSTATUS(status);//report the error of my child to my parent
-            }
-        }
-        int result = execute(memory2);
-        dup2(save_read_fd, 0);
-        return result;
-    }else{//child (write)
-        close(fd_pipe[0]);
-        dup2(fd_pipe[1], 1);
-        close(fd_pipe[1]);
-        exit(execute(memory1));//report the result to parent who is waiting
-    }
-    return 0;
-}
+
 int execute(tsh_memory *memory){
     if(strstr(memory->comand, "|")==NULL){//Pas de pipe
         if((returnval = execSimpleCommande(memory)))printError(memory, returnval);
