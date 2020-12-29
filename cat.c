53722b144a2e586a1e4067e7afc6d863bc8aca10
djeljeli <yjel11@laposte.net>
2020-12-29 19:22:17 +0100

    modification cat command 3

--- cat.c
+++ cat.c
@@ -15,7 +15,7 @@
 tsh_memory old_memory;
 
 
-int stop = 0; // stop the cat when giver without argument
+int stopCat = 0; // stop the cat when giver without argument
 
 void cat_in_tar(int fd, char* PATH){ //test tar
     lseek(fd,0,SEEK_SET);
@@ -38,4 +38,36 @@ void cat_in_tar(int fd, char* PATH){ //test tar
     }
     
     
+}
+
+//case of the cat where the user gives no arguement
+void exitFromCat(int signal){
+    int pipe_fd[2];
+    pipe(pipe_fd);
+    int pid = fork();
+    char buffer[1024];
+    memset(buffer,0,1024);
+    if(pid == 0){
+        close(pipe_fd[0]);
+        while(read(0,buffer,1024) > && stopCat == 0){
+            write(pipe_fd[1], buffer, strlen(buffer));
+            memset(buffer,0,1024);
+        }
+        close(pipe_fd[1]);
+        exit(0);
+        
+    }
+    else{
+        close(pipe_fd[1]);
+        while(read(pipe_fd[0],buffer,1024) > 0 && stopCat == 0){
+            write(1,buffer,1024);
+            memset(buffer,0,1024);
+
+        }
+        close(pipe_fd[0])
+
+        //redefining SIGINT singal behaviour to default
+        sigaction(SIGINT,&old_cat,NULL);
+        return 1;
+    }
 }
