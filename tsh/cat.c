e312f8cbeb6fa6006e27f37262c03bf6eec2834b
Nicolas Sangsiri <nicolassangsiri@gmail.com>
2020-12-30 19:59:27 +0100

    Added & fixed cat.c

--- tsh/cat.c
+++ tsh/cat.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+#include <wait.h>
+#include <fcntl.h> 
 #include <signal.h>
 
 #include "exec_funcs.h"
@@ -13,6 +11,7 @@
 #include "string_traitement.h"
 #include "cd.h"
 
+tsh_memory old_memory; //will be use to save/restore a memory
 char **array_execvp;
 
 
@@ -29,12 +28,12 @@ void cat_in_tar(int fd, char* PATH){ //test tar
         int nb_bloc_fichier = (file_s + 512 -1) / 512;
         if(strcmp(hd.name,PATH)==0){
             char buffer[nb_bloc_fichier*512];
-            read(fd, buffer, BLOCKSIZE);
+            read(fd, buffer, BLOCKSIZE*nb_bloc_fichier);
             write(1,buffer,strlen(buffer));
         }
 
         //next header block
-        lseek(fd,nb_bloc_fichier*512,SEEK_SET);
+        lseek(fd,nb_bloc_fichier*512,SEEK_CUR);
 
     }
     
@@ -56,35 +55,18 @@ void exitFromCat(int signal){
 
 //case of the cat where the user gives no arguement
 int exec_cat(struct sigaction old_act){
-    int pipe_fd[2];
-    pipe(pipe_fd);
     int pid = fork();
-    char buffer[1024];
-    memset(buffer,0,1024);
     if(pid == 0){
-        close(pipe_fd[0]);
-        while(read(0,buffer,1024) > 0 && stopCat == 0){ // don't work
-            write(pipe_fd[1], buffer, strlen(buffer));
-            memset(buffer,0,1024);
-        }
-        close(pipe_fd[1]);
+        execlp("cat", "cat", NULL);
         exit(0);
-        
     }
     else{
-        close(pipe_fd[1]);
-        while(read(pipe_fd[0],buffer,1024) > 0 && stopCat == 0){
-            write(1,buffer,1024);
-            memset(buffer,0,1024);
-
-        }
-        close(pipe_fd[0]);
-
-        //redefining SIGINT singal behaviour to default
-
-        //sigaction(SIGINT,&old_cat,NULL);
-        return 1;
+        waitpid(pid, NULL, 0); 
+        write(1, "\n", 1);
     }
+    //redefining SIGINT singal behaviour to default
+    sigaction(SIGINT,&old_act,NULL);
+    return 1;
 }
 
 
---
+++ tsh/cat.h
@@ -0,0 +1,7 @@
+#ifndef CAT_H 
+#define CAT_H
+
+#include "tsh_memory.h"
+
+extern int cat(tsh_memory *memory, char args[50][50], int nb_arg, char option[50][50], int nb_opt);
+#endif
